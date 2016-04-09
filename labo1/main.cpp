86449ab54ebf60f38812231b822c516f83e4ef87
Basile Vu <basile.vu@gmail.com>
2016-04-10 01:16:01 +0200

    Foctorizing tests

--- labo1/main.cpp
+++ labo1/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <random>
+#include <vector>
+#include <string>
 
 #include "HitOrMiss.h"
 #include "MixedGeometric.h"
@@ -11,52 +13,34 @@ using namespace std;
 int main() {
 
     using namespace std;
+    typedef pair<vector<double>, vector<double>> Dataset;
 
     seed_seq seed = {42, 42, 42};
-    size_t nSim = 10000000;
+    size_t nSim = 100000;
 
     cout << "Format : " << endl;
     cout << "-- <Ensemble de donnees> --" << endl;
     cout << "<Methode> : <esperance empirique>, <temps>s" << endl;
     cout << endl;
 
-    {
-        cout << "-- Uniforme (5,1) (15, 1) --" << endl;
-        vector<double> xs = {5, 15};
-        vector<double> ys = {1, 1};
+    vector<Dataset> dataSets = {
+            {{5, 15}, {1,1}},
+            {{2, 3, 7, 10, 14, 15}, {0, 1, 0, 0, 1, 0}},
+            {{2, 4, 7, 9, 12, 13, 17, 20}, {8, 10, 10, 9, 5, 9, 10, 6}},
+            {{2, 3, 5, 10, 12, 13, 15, 17, 19, 20}, {1, 10, 0, 1, 8, 4, 1, 0, 2, 9}}
+    };
 
-        Benchmarker(HitOrMiss(xs, ys, seed)).run("Acceptation - rejet    ", nSim);
-        Benchmarker(MixedGeometric(xs, ys, seed)).run("Melanges - geometrique ", nSim);
-        Benchmarker(MixedInverse(xs, ys, seed)).run("Melanges - inverses    ", nSim);
-        cout << endl;
-    }
+    vector<string> datasetNames = {
+        "Uniforme",
+        "Deux triangulaires",
+        "Profil plutot plat",
+        "Profil accidente"
+    };
 
-    {
-        cout << "-- Deux triangulaires --" << endl;
-        vector<double> xs = {2, 3, 7, 10, 14, 15};
-        vector<double> ys = {0, 1, 0, 0, 1, 0};
-
-        Benchmarker(HitOrMiss(xs, ys, seed)).run("Acceptation - rejet    ", nSim);
-        Benchmarker(MixedGeometric(xs, ys, seed)).run("Melanges - geometrique ", nSim);
-        Benchmarker(MixedInverse(xs, ys, seed)).run("Melanges - inverses    ", nSim);
-        cout << endl;
-    }
-
-    {
-        cout << "-- profil plutot plat --" << endl;
-        vector<double> xs = {2, 4, 7, 9, 12, 13, 17, 20};
-        vector<double> ys = {8, 10, 10, 9, 5, 9, 10, 6};
-
-        Benchmarker(HitOrMiss(xs, ys, seed)).run("Acceptation - rejet    ", nSim);
-        Benchmarker(MixedGeometric(xs, ys, seed)).run("Melanges - geometrique ", nSim);
-        Benchmarker(MixedInverse(xs, ys, seed)).run("Melanges - inverses    ", nSim);
-        cout << endl;
-    }
-    
-    {
-        cout << "-- Profil accidente --" << endl;
-        vector<double> xs = {2, 3, 5, 10, 12, 13, 15, 17, 19, 20};
-        vector<double> ys = {1, 10, 0, 1, 8, 4, 1, 0, 2, 9};
+    for (size_t i = 0; i < dataSets.size(); ++i) {
+        cout << "-- " + datasetNames[i] << " --" << endl;
+        vector<double>& xs = dataSets[i].first;
+        vector<double>& ys = dataSets[i].second;
 
         Benchmarker(HitOrMiss(xs, ys, seed)).run("Acceptation - rejet    ", nSim);
         Benchmarker(MixedGeometric(xs, ys, seed)).run("Melanges - geometrique ", nSim);
